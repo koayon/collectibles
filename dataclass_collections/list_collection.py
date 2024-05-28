568cfcd8b96fc7890f21b5e28fa3d9c9add6cac1
Kola Ayonrinde <koayon@gmail.com>
2024-05-28 19:22:42 +0200

    Initial commit

---
+++ dataclass_collections/list_collection.py
@@ -0,0 +1,41 @@
+from dataclasses import fields
+from typing import Generic, TypeVar
+
+from typeguard import check_type
+
+T = TypeVar("T")
+
+
+class ListCollection(list[T], Generic[T]):
+    def __init__(self, args: list[T]):
+        if args:
+            self.underlying_type = type(args[0])
+
+            for item in args:
+                if not isinstance(item, self.underlying_type):
+                    raise TypeError(
+                        f"""All elements in the ListCollection must be instances of the same type.
+Here, the first element is of type {self.underlying_type}, but there's an element of type {type(item)}"""
+                    )
+            check_type("args", args, list[self.underlying_type])
+
+        super().__init__(args)
+        self._add_properties()
+
+    def _add_properties(self):
+        cls = self.__class__
+        dataclass_type = self.__orig_bases__[0].__args__[0]  # type: ignore
+        # TODO: ^Can we switch this with self.underlying_type?
+        for field in fields(dataclass_type):
+            name = field.name
+            property_type = list[field.type]
+            setattr(cls, name, self._make_property(name, property_type))
+
+    def _make_property(self, name: str, typ: type):
+        def prop(self):
+            # Intuitively [item.name for item in self]
+            # But the following is a little nicer
+            return [getattr(item, name) for item in self]
+
+        prop.__name__ = name
+        return property(prop)
